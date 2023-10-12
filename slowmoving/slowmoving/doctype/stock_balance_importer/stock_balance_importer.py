a604c83d16f53f8b47a30592b054cd08f9846123
hrishi2003 <93807312+hrishi2003@users.noreply.github.com>
2023-10-12 21:16:05 +0530

    Update stock_balance_importer.py

--- slowmoving/slowmoving/doctype/stock_balance_importer/stock_balance_importer.py
+++ slowmoving/slowmoving/doctype/stock_balance_importer/stock_balance_importer.py
@@ -248,6 +248,6 @@ def make_entries(file_name,warehouse_name,doc):
 
 @frappe.whitelist()
 def make_st_ent(file_name,warehouse_name,doc):
-	frappe.enqueue(make_entries, queue='long',timeout=4000,file_name=file_name,warehouse_name=warehouse_name,doc=doc)
+	frappe.enqueue(make_entries, queue='long',timeout=15000,file_name=file_name,warehouse_name=warehouse_name,doc=doc)
 
 
