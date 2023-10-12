8d9048e6e64c9c2f4c695979c0b72cf1d64eb767
hrishi2003 <93807312+hrishi2003@users.noreply.github.com>
2023-10-12 22:48:49 +0530

    Update stock_balance_importer.js

--- slowmoving/slowmoving/doctype/stock_balance_importer/stock_balance_importer.js
+++ slowmoving/slowmoving/doctype/stock_balance_importer/stock_balance_importer.js
@@ -1,35 +1,35 @@
 // Copyright (c) 2022, frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Stock Balance Importer', {
-	refresh: function(frm) {
-		frm.add_custom_button("Stock Balance Data", 
-	 		() => frm.events.import_data(frm)
-        );
-	},
-	 import_data : function(frm) {
-	 	frappe.call({
-				"method":"slowmoving.slowmoving.doctype.stock_balance_importer.stock_balance_importer.make_st_ent",
-				"args":{
-					"file_name": frm.doc.ise_file,
-					"warehouse_name" : frm.doc.warehouse,
-					"doc" : frm.doc.name
-				},
-				freeze: true,
-				callback:function(r){
-					console.log('GHJJHJHJHJBBJ')
-					if(!r.exc) {
-    					if(r.message) {
-   							frappe.msgprint("done")
-                         }}
-						}
-        	   })
+// frappe.ui.form.on('Stock Balance Importer', {
+// 	refresh: function(frm) {
+// 		frm.add_custom_button("Stock Balance Data", 
+// 	 		() => frm.events.import_data(frm)
+//         );
+// 	},
+// 	 import_data : function(frm) {
+// 	 	frappe.call({
+// 				"method":"slowmoving.slowmoving.doctype.stock_balance_importer.stock_balance_importer.make_st_ent",
+// 				"args":{
+// 					"file_name": frm.doc.ise_file,
+// 					"warehouse_name" : frm.doc.warehouse,
+// 					"doc" : frm.doc.name
+// 				},
+// 				freeze: true,
+// 				callback:function(r){
+// 					console.log('GHJJHJHJHJBBJ')
+// 					if(!r.exc) {
+//     					if(r.message) {
+//    							frappe.msgprint("done")
+//                          }}
+// 						}
+//         	   })
 
-	 },
-	 download_template(frm) {
-		open_url_post(frappe.request.url, {
-			cmd: 'frappe.core.doctype.file.file.download_file',
-			file_url: '/private/files/Sample Upload Template (1).xlsx'
-		});
-	},
-});
+// 	 },
+// 	 download_template(frm) {
+// 		open_url_post(frappe.request.url, {
+// 			cmd: 'frappe.core.doctype.file.file.download_file',
+// 			file_url: '/private/files/Sample Upload Template (1).xlsx'
+// 		});
+// 	},
+// });
