2e0f7ad6e0ac41816efadddda01170354d9396e4
hrishi2003 <93807312+hrishi2003@users.noreply.github.com>
2023-10-13 16:31:27 +0530

    Update stock_balance_importer.py

--- slowmoving/slowmoving/doctype/stock_balance_importer/stock_balance_importer.py
+++ slowmoving/slowmoving/doctype/stock_balance_importer/stock_balance_importer.py
@@ -112,15 +112,13 @@ def make_entries(file_name,warehouse_name,doc):
 				stc_bal.save()
 
 		else:
-			frappe.log_error(f'd, {d}')
 			for y in l:
 				if y in ic:
 					if y == (ise_file.cell(row=r,column=2)).value:
-						frappe.log_error(f'yyyyy{y}')
-						sb = frappe.get_doc("Stock Balance Form",{'item_code':y,'warehouse':warehouse_name})
-						sb.balance_qty = (ise_file.cell(row=r,column=5)).value
-						frappe.log_error(f'balance_qty{sb.balance_qty}')
-						sb.save()
+						if frappe.db.exists("Stock Balance Form", {"item_code": (ise_file.cell(row=r,column=2)).value,'warehouse':warehouse_name,"balance_qty":['!=',(ise_file.cell(row=r,column=5)).value]}):
+							sb = frappe.get_doc("Stock Balance Form",{'item_code':y,'warehouse':warehouse_name})
+							sb.balance_qty = (ise_file.cell(row=r,column=5)).value
+							sb.save()
 				else:
 					if y:
 						frappe.log_error('Deteted ROW')
